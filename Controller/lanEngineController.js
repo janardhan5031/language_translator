e0cddf077fefd248f738088c4d41be8b290fdfe8
janardhan5031 <janardhan5031>
2022-12-28 23:54:57 +0530

    multi level translation with iso language code

--- Controller/lanEngineController.js
+++ Controller/lanEngineController.js
@@ -1,7 +1,5 @@
 // Imports the Google Cloud client library
-const translate = require('translate');
-translate.engine = 'google';
-translate.key = '';
+const google_translate = require('google-translate')(process.env.API_KEY);
 
 
 exports.translateWord = async (req, res, next) => {
@@ -10,10 +8,17 @@ exports.translateWord = async (req, res, next) => {
 
     try {
         
-        const converted = await(translate(text, language));
-        console.log(converted);
-    
-        res.send(converted);
+        google_translate.translate(text, language, (err, converted_Text) => {
+            
+            if (err) {
+                console.log(err)
+                res.send('please enter correct language code')
+                return;
+            }
+            console.log(converted_Text);
+        
+            res.send(converted_Text);
+        })
     } catch (err) {
         console.log(err)
     }
