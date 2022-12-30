027b77384ddafd854cbe942ac1adbe59cd4514dd
janardhan5031 <janardhan5031>
2022-12-30 14:01:18 +0530

    validating the input fields before call the server to translate

--- 01_FRONTEND/frontend/src/components/Lang_Translate/Language_trans.js
+++ 01_FRONTEND/frontend/src/components/Lang_Translate/Language_trans.js
@@ -4,29 +4,52 @@ import Options from "./Options";
 
 
 const Language_Translator = () => {
-    const [isOpen, setIsOpen] = useState(false)
+    const [isFromCodeValid, setIsFromCodeValid] = useState(false)
+    const [isToCodeValid, setIsToCodeValid] = useState(false)
+
+    function fromCodeChangeHandler(e) {
+        const fromCode = e.target.value;
+        console.log(fromCode)
+        if (fromCode !== 'from') {
+            setIsFromCodeValid(true)
+        }
+
+    }
+
+    function ToCodeHandler(e) {
+        const toCode = e.target.value;
+        if (toCode !== 'to') {
+            setIsToCodeValid(true)
+        }
+
+    }
 
     const submitHandler = (e) => {
         e.preventDefault();
 
         // console.log(e.target.from_text.value ,e.target.from_code.value)
+        
+        const from_text = e.target.from_text.value.trim();
         const from_code = e.target.from_code.value;
         const to_code = e.target.to_code.value;
-        const from_text = e.target.from_text.value;
 
-        // console.log(from_code,to_code,from_text)
-        
-        
+        if (isFromCodeValid && isToCodeValid) {
+            console.log(from_code,to_code,from_text)
+        }   
+        else {
+            window.alert('plaese select the languages to change')
+        }
+
     }
 
     return (<Form onSubmit={submitHandler} className='m-4'>
         <Row className="mb-3">
             <Stack direction='horizontal' gap={3}>
-                <Form.Select id='from_code'>
+                <Form.Select id='from_code' onChange={fromCodeChangeHandler}>
                     <option>from</option>
                     <Options />
                 </Form.Select>
-                <Form.Select id='to_code'>
+                <Form.Select id='to_code' onChange={ToCodeHandler}>
                     <option>to</option>
                     <Options />
                 </Form.Select>
